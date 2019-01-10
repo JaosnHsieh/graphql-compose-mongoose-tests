bf3a1d67aa3df4d5b6193a35d2e7c8cd57640842
jasonhsieh <jasonhsieh@ghtinc.com>
2019-01-10 17:58:24 +0800

    update example3 to use "graphql" printSchema method

--- example3/index.js
+++ example3/index.js
@@ -1,12 +1,13 @@
 /**
- * print type from GraphQLObjectType according to
+ * print schema and type from GraphQLObjectType according to
  * 1. https://github.com/graphql-compose/graphql-compose-mongoose#can-i-get-generated-vanilla-graphql-types
  * 2. https://github.com/apollographql/graphql-tools/issues/398#issue-256044531
+ * 3. https://github.com/graphql/graphql-js/issues/1464#issuecomment-412506816
  */
 import mongoose from "mongoose";
 import { composeWithMongoose } from "graphql-compose-mongoose";
 import { schemaComposer } from "graphql-compose";
-import { graphql, printType } from "graphql";
+import { graphql, printSchema, printType } from "graphql";
 
 const UserSchema = new mongoose.Schema(
   {
@@ -23,4 +24,22 @@ const User = mongoose.model("User", UserSchema);
 const customizationOptions = {};
 const UserTC = composeWithMongoose(User, customizationOptions);
 
-console.log(printType(UserTC.getType()));
+schemaComposer.Query.addFields({
+  userById: UserTC.getResolver("findById"),
+  userByIds: UserTC.getResolver("findByIds"),
+  userOne: UserTC.getResolver("findOne")
+});
+
+schemaComposer.Mutation.addFields({
+  userCreateOne: UserTC.getResolver("createOne"),
+  userUpdateById: UserTC.getResolver("updateById"),
+  userUpdateOne: UserTC.getResolver("updateOne")
+});
+
+const schema = schemaComposer.buildSchema();
+
+console.log("-----printSchema(schema)-----", printSchema(schema));
+console.log(
+  "-----printType(UserTC.getType())-----",
+  printType(UserTC.getType())
+);
